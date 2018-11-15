c4e40bab560d7dbdefea1b3e8bfbfd2d20048b7a
Aron <17043506@student.hhs.nl>
2018-11-15 13:30:16 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace week1opdr2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
