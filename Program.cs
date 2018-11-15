beebf244d3b353cf51544d1d1e95627302ae731b
Aron <17043506@student.hhs.nl>
2018-11-15 13:48:56 +0100

    Opdracht opgezet
    opdracht opgezet

--- Program.cs
+++ Program.cs
@@ -7,6 +7,17 @@ namespace week1opdr2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Question q1 = new Question();
+            q1.setText("Who was the inventor of Java?");
+            q1.setAnswer("James Gosling");
+
+            q1.display();
+            Console.WriteLine("Your answer: ");
+            var antwoord = Console.ReadLine();
+            
+            Console.WriteLine(q1.checkAnswer(antwoord));
+            Console.ReadLine();
         }
     }
 }
---
+++ Question.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace week1opdr2
+{
+    class Question
+    {
+        private String text;
+        private String answer;
+
+        //set empty question
+        public Question()
+        {
+            text = "";
+            answer = "";
+        }
+
+        /**
+        Sets the question text.
+        @param questionText the text of this question
+        */
+        public void setText(String questionText)
+        {
+            text = questionText;
+        }
+
+        /**
+        Sets the answer for this question.
+        @param correctResponse the answer
+        */
+        public void setAnswer(String correctResponse)
+        {
+            answer = correctResponse;
+        }
+
+        /**
+        Checks a given response for correctness.
+        @param response the response to check
+        @return true if the response was correct, false otherwise
+        */
+        public bool checkAnswer(String response)
+        {
+            return response.Equals(answer);
+        }
+
+        /**
+        Displays this question.
+        */
+        public void display()
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
